a406d04ffe1f1de3f52fba00aa3e87db6ed22da4
Frida Ericsson <fridaericsson@live.com>
2024-12-06 15:11:04 +0100

    added a navbar

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,9 +1,22 @@
+import { Link } from 'react-router-dom'
 import './navbar.css'
 
-function navbar() {
-  return (
-    <div>navbar</div>
-  )
+const Navbar: React.FC = () => 
+    {
+return (
+    <div className="navbar-container">
+        <div className="navbar-content">
+            <div className="navbar-brand">
+                <h2>ReceptPlaneraren</h2>
+            </div>
+            <div className="navbar-list">
+                <Link to="/explore">Utforska</Link>
+                <span>|</span>
+                <span>|</span>
+                <Link to="/signup"> Bli medlem</Link>
+            </div>
+        </div>
+    </div>
+)
 }
-
-export default navbar
+export default Navbar
