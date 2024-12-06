30f79b2661519094a6cb3e53cafee9f4337b0338
Frida Ericsson <fridaericsson@live.com>
2024-12-06 15:09:40 +0100

    added changes in router to get navbar and footer to work on all pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Footer from './components/footer/footer'
-import AppRouter from './router/AppRouter'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AppRouter from './router/AppRouter';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter />
-    <Footer />
   </StrictMode>,
-)
+);
--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from '../pages/landing';
+import Explore from '../pages/explore';
+import SignUp from '../pages/signUp';
+import Navbar from '../components/navbar/navbar';
+import Footer from '../components/footer/footer';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
