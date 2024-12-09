cd7ff360ebaf3a151de2929db4790fbc4bb6a620
Frida Ericsson <fridaericsson@live.com>
2024-12-09 17:40:31 +0100

    added login modal

--- frontend/src/components/loginModal/loginModal.tsx
+++ frontend/src/components/loginModal/loginModal.tsx
@@ -1,9 +1,54 @@
+import { useState } from 'react'
 import './loginModal.css'
 
-function loginModal() {
-  return (
-    <div>loginModal</div>
-  )
+interface LoginModalProps {
+    isOpen: boolean;
+    onClose: () => void
 }
 
-export default loginModal
+const LoginModal: React.FC<LoginModalProps> = ({isOpen, onClose }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    if (!isOpen) return null
+    // Handle form submission
+    const handleLogin = (e: React.FormEvent) => {
+      e.preventDefault();
+      onClose(); 
+    };
+  
+    return (
+      <div className="modal-overlay">
+        <div className="modal-content">
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className='login-btn'>Login</button>
+          </form>
+          <button className="close-btn" onClick={onClose}>Close</button>
+        </div>
+      </div>
+    );
+  };
+  
+
+export default LoginModal
